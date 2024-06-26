afd83812c5bd51fb3b9317bc1da8190d8657e74c
alefytaas <taas.sousa@gmail.com>
2024-06-26 15:13:01 -0300

    Projeto com as funções funcionando. FALTA O MENU

--- biblioteca.rb
+++ biblioteca.rb
@@ -14,7 +14,6 @@ class Biblioteca
             cod_livro = (0...50).map { ('a'..'z').to_a[rand(4)] }.join
         end
         @livros[cod_livro] = livro
-        p livro.genero
         adicionar_genero(livro.genero, cod_livro)
         adicionar_autor(livro.autor, cod_livro)
         adicionar_titulo(livro.titulo, cod_livro)
@@ -23,42 +22,69 @@ class Biblioteca
     
 
     def mostrar_livros_por_genero(genero)
-        puts genero
-        puts @genero
-        puts @genero.has_key?(genero)
+        puts "\n Livros do Genero: #{genero}"
         if @genero.has_key?(genero)
-            @genero[genero].each do |generos|
-                generos.each do |elemento|
+                for elemento in @genero[genero]           
                 puts "#{@livros[elemento].titulo} - #{@livros[elemento].autor} - #{@livros[elemento].genero}"
                 end
-            end
+            
         else
             puts "Genero não encontrado"
         end
 
     end
+    def mostrar_livros_por_autor(autor)
+        puts "\n Livros do Autor: #{autor}"
+        if @autor.has_key?(autor)
+                for elemento in @autor[autor]           
+                puts "#{@livros[elemento].titulo} - #{@livros[elemento].autor} - #{@livros[elemento].genero}"
+                end
+            
+        else
+            puts "Autor não encontrado"
+        end
+
+    end
+
+    def mostrar_livros_por_titulo(titulo)
+        achou = false
+        puts "\n Livros que contem no Titulo: #{titulo}"
+        @titulo.each_key do |key|
+            if key.include?(titulo)
+                achou = true
+                for elemento in @titulo[key]           
+                    puts "#{@livros[elemento].titulo} - #{@livros[elemento].autor} - #{@livros[elemento].genero}"
+                    
+                end
+            end
+        end
+        if achou == false
+            puts "Livro não encontrado"    
+        
+        end
+    end
     private
     def adicionar_genero(genero, cod_livro)
         if @genero.has_key?(genero)
-            @genero[genero] << cod_livro
-          else
-              @livros[genero] ||= [cod_livro]
-          end
+           @genero[genero] << cod_livro
+        else
+            @genero[genero] ||= [cod_livro]
+        end
     end
     
     def adicionar_autor(autor, cod_livro)
-        if @genero.has_key?(autor)
-            @genero[autor] << cod_livro
+        if @autor.has_key?(autor)
+            @autor[autor] << cod_livro
           else
-              @livros[autor] ||= [cod_livro]
+              @autor[autor] ||= [cod_livro]
           end
     end
     
     def adicionar_titulo(titulo, cod_livro)
-        if @genero.has_key?(titulo)
-            @genero[titulo] << cod_livro
+        if @titulo.has_key?(titulo)
+            @titulo[titulo] << cod_livro
           else
-              @livros[titulo] ||= [cod_livro]
+              @titulo[titulo] ||= [cod_livro]
           end
     end
     end
--- main.rb
+++ main.rb
@@ -5,9 +5,19 @@ cabana = Livro.new("Engerenharia de Software Descomplicada", "Mauricio Aniche", 
 teste_e_design = Livro.new("Engerenharia de Software complicada", "Mauricio Anichetes", :EngenhariaDeSoftware)
 outro_livro = Livro.new("Prgramação WEB com Ruby", "Marco Doido", "teste")
 
+livro1 = Livro.new("Ruby Programming", "Autor A", "Tecnologia")
+livro2 = Livro.new("Aprendendo Ruby", "Autor B", "Tecnologia")
+livro3 = Livro.new("Romance em Ruby", "Autor C", "Ficção")
+
+
 biblioteca = Biblioteca.new
 biblioteca.adicionar_livro(cabana)
 biblioteca.adicionar_livro(teste_e_design)
 biblioteca.adicionar_livro(outro_livro)
+biblioteca.adicionar_livro(livro1)
+biblioteca.adicionar_livro(livro2)
+biblioteca.adicionar_livro(livro3)
 
-biblioteca.mostrar_livros_por_genero("teste")
+biblioteca.mostrar_livros_por_genero(:EngenhariaDeSoftware)
+biblioteca.mostrar_livros_por_autor("Autor A")
+biblioteca.mostrar_livros_por_titulo("Ruby")
